d37f1ad44ce413a6929dbe567887dd0f3a39f18d
Jur Kroeze <jurkrz@gmail.com>
2017-08-27 15:06:46 +0200

    JurAI base for startPos()

--- JurAI.cpp
+++ JurAI.cpp
@@ -45,65 +45,12 @@ Start_pos JurAI::startPos()
 
 int JurAI::evaluate_tile(Tile target, int piece_falue)
 {
-	if (target.piece.owner == playerNumber){ return -900; }
-	if (target.land == 'W'){ return -900; }
-	if (target.piece.owner != playerNumber){ return 10; }
-	if (target.piece.value == -1){ return 1; }
-	else { return 0; }
+	return 0;
 }
 
 Move JurAI::move(Tile field[10][10], Move opponent_move)
 {
 	Move output;
-
-	//update de hasmoved map
-	if (opponent_move.x != -1){
-		int opp_des_x, opp_des_y;
-		if (opponent_move.cardinal == 'N'){ opp_des_x = opponent_move.x; opp_des_y = opponent_move.y - 1; }
-		else if (opponent_move.cardinal == 'E'){ opp_des_x = opponent_move.x + 1; opp_des_y = opponent_move.y; }
-		else if (opponent_move.cardinal == 'S'){ opp_des_x = opponent_move.x; opp_des_y = opponent_move.y + 1; }
-		else if (opponent_move.cardinal == 'W'){ opp_des_x = opponent_move.x - 1; opp_des_y = opponent_move.y; }
-		hasmoved[opp_des_x][opp_des_y] = 1;
-	}
-
-	int max = -10000, rating;
-	char best = 'N';
-	int best_tile[2] = {-1, -1};
-	for (int T1 = 0; T1 < 10; T1++){
-		for (int T2 = 0; T2 < 10; T2++){
-			if (field[T1][T2].piece.owner == playerNumber && field[T1][T2].piece.value != 0)//evaluate the moves of the piece it the Ai ownse it and it is not a flag or a bom
-			{
-				Tile target;
-				if (T1 != 0){
-					target = field[T1 - 1][T2];// get the target field to evaluate
-					rating = evaluate_tile(target, field[T1][T2].piece.value);// get the avaluation
-					if (rating > max){ max = rating;  best = 'N'; best_tile[0] = T1; best_tile[1] = T2; }// if it is max move there
-				}
-
-				if (T1 != 10){
-					target = field[T1 + 1][T2];
-					rating = evaluate_tile(target, field[T1][T2].piece.value);
-					if (rating > max){ max = rating;  best = 'S'; best_tile[0] = T1; best_tile[1] = T2; }
-				}
-
-				if (T2 != 0){
-					target = field[T1][T2 - 1];
-					rating = evaluate_tile(target, field[T1][T2].piece.value);
-					if (rating > max){ max = rating;  best = 'W'; best_tile[0] = T1; best_tile[1] = T2; }
-				}
-
-				if (T1 != 10){
-					target = field[T1][T2 + 1];
-					rating = evaluate_tile(target, field[T1][T2].piece.value);
-					if (rating > max){ max = rating;  best = 'E'; best_tile[0] = T1; best_tile[1] = T2; }
-				}
-			}
-		}
-	}
-	output.cardinal = best;
-	output.x = best_tile[1];
-	output.y = best_tile[0];
-	printf("AI%i: %i, %i, %c\n", playerNumber, output.x, output.y, output.cardinal);
 	return output;
 }
 
