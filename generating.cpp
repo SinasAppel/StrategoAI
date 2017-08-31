b5e621b2cb3ee1f1695a62a123ab037a0725eecf
vagegast37 <sanderdamsma@gmail.com>
2017-08-31 19:26:51 +0200

    Update generating.cpp

--- generating.cpp
+++ generating.cpp
@@ -6,66 +6,6 @@
 #include "generating.h"
 #include "AI1.h"
 
-// Standard constructor for a Piece, creates an empty piece.
-Piece::Piece() {
-	value = -1;
-	name = 'E';
-	owner = 0;
-	visible = false;
-}
-
-/**
- * Special constructor for Piece.
- * Takes a char n, that denotes the name of the Piece.
- * For example '5' corresponds to the Five (AKA Lieutenant) Piece.
- */
-Piece::Piece(char n, int o) {
-	name = n;
-	owner = o;
-	visible = false;
-	if (n == 'B' || n == 'F') { // Bomb or flag, both have the same value
-		value = 0;
-	} else if (n == 'T') { // Ten (AKA Marshal)
-		value = 10;
-	} else { // Name equals value for all other pieces
-		value = name - '0'; // Handy trick to convert chars to ints
-	}	
-}
-
-/**
- * Special constructor for Piece
- * Takes an int v, that denotes the value of the Piece
- * In this case, 0 is the flag and 11 is the bomb
- */
-Piece::Piece(int v, int o) {
-	v > 10 ? value = 0 : value = v;
-	owner = o;
-	visible = false;
-	if (v == 11) {
-		name = 'B';
-	} else if (v == 0) {
-		name = 'F';
-	} else if (v == 10) {
-		name = 'T';
-	} else {
-		name = value + '0';
-	}
-}
-
-// Default constructor for a Grass tile
-Grass::Grass() {
-	land = 'G';
-	Piece p;
-	piece = p;
-}
-
-// Default constructor for a Water tile
-Water::Water() {
-	land = 'W';
-	Piece p;
-	piece = p;
-}
-
 //Creates the initial empty board
 void createBoard(Tile field[10][10])
 {
@@ -104,7 +44,12 @@ void fillBoard(Tile field[10][10], Start_pos startPos1, Start_pos startPos2)
 */
 Tile cleanGrassTile()
 {
-	Grass g;
+	Tile g;
+	g.land = 'G';
+	g.piece.value = -1;
+	g.piece.name = 'E';
+	g.piece.owner = 0;
+	g.piece.visible = true;
 	return g;
 }
 
@@ -113,6 +58,11 @@ Tile cleanGrassTile()
 */
 Tile cleanWaterTile()
 {
-	Water w;
+	Tile w;
+	w.land = 'W';
+	w.piece.value = -1;
+	w.piece.name = 'E';
+	w.piece.owner = 0;
+	w.piece.visible = true;
 	return w;
 }
