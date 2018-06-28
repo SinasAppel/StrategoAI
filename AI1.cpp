6cd98b8e59072197305d9dae42b5d0fd35cf1278
Jur Kroeze <jur.kroeze@DEV-050.dev.kantoor.mobielwerkt.nl>
2018-06-28 17:12:39 +0200

    Include the definitions file in all the needed files

--- AI1.cpp
+++ AI1.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <ctime>
 #include "generating.h"
+#include "definitions.cpp"
 #include "AI1.h"
 using namespace std;
 
--- JurAI.cpp
+++ JurAI.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <ctime>
 #include "generating.h"
+#include "definitions.cpp"
 #include "JurAI.h"
 using namespace std;
 
